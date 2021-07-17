4aee6f0713fbeeb05b4a5009e57d19697b410222
marnixah <marnix@hagehoeve.com>
2021-07-18 01:32:35 +0200

    feat(findClosest): slightly more varied responses

--- src/lib/findClosest.ts
+++ src/lib/findClosest.ts
@@ -5,17 +5,20 @@ import Quote from '../interfaces/Quote'
 function findClosest(things: Quote[], emotionState: Emotions): Quote
 function findClosest(things: Emoji[], emotionState: Emotions): Emoji
 function findClosest(things: Quote[] | Emoji[], emotionState: Emotions) {
-  let minimumDistance = 100
-  let minimum = things[0]
-  for (let i = 0; i < things.length; i++) {
-    const thing = things[i]
-    const distance = calculateDistance(emotionState, thing.emotions)
-    if (minimumDistance < distance) continue
-    minimum = thing
-    minimumDistance = distance
-  }
-  return minimum
+  let arr = getDistanceArray(things, getDistanceArray(things, emotionState, 100)[0].emotions, 0.3)
+  return randomChoice(arr)
 }
+const randomChoice = (arr: any[]) => arr[Math.floor(Math.random() * arr.length)]
+
+const getDistanceArray = (things: Quote[] | Emoji[], emotionState: Emotions, maxDistance: number) =>
+  things
+    .map((thing: Quote | Emoji) => ({
+      ...thing,
+      distance: calculateDistance(emotionState, thing.emotions),
+    }))
+    .filter((thing) => thing.distance < maxDistance)
+    .sort((a, b) => a.distance - b.distance)
+
 const calculateDistance = (emotion1: Emotions, emotion2: Emotions) => {
   const A = Math.pow(Math.abs(emotion1.focusLevel - emotion2.focusLevel), 2)
   const B = Math.pow(Math.abs(emotion1.frustration - emotion2.frustration), 2)
